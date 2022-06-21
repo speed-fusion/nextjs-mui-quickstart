9f334bbb44b0bccde8d327e0c289a7855e5e60bd
speed-fusion <106366121+speed-fusion@users.noreply.github.com>
2022-06-21 16:48:55 +0530

    done

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,38 @@ import React from 'react';
 import { CacheProvider } from '@emotion/react';
 import PropTypes from 'prop-types';
 import createEmotionCache from '../src/createEmotionCache';
-// import Layout from '../components/layout';
+import Layout from '../components/layout';
+import Navbar from '../components/navbar';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { getTheme } from '../theme/theme';
 
 const clientSideEmotionCache = createEmotionCache();
 
 const MyApp = (props) => {
  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
+ const [mode, setMode] = React.useState('light');
+
+ const colorMode = React.useMemo(
+  () => ({
+    toggleColorMode: () => {
+      setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+    },
+  }),
+  [],
+);
+
  return (
+   
    <CacheProvider value={emotionCache}>
      {/* <ThemeProvider theme={theme}>
        <CssBaseline /> */}
-       {/* <Layout> */}
+       <ThemeProvider theme={getTheme(mode)}>
+       <CssBaseline />
+       <Layout>
        <Component {...pageProps} />
-       {/* </Layout> */}
+       </Layout>
+       </ThemeProvider>
      {/* </ThemeProvider> */}
    </CacheProvider>
  );
